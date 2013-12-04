615ba242dbce91c991cfee1f1fc29edb7f003907
Kachenbach <kachenba@my.hpu.edu>
2013-12-04 10:53:12 -1000

    assests

---
+++ Assets/Scripts/PlayerController.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+var speed : float;
+var countText : GUIText;
+var winText : GUIText;
+private var count : int;
+
+function Start() {
+	count = 0;
+	SetCountText();
+	winText.text = "";
+
+}
+
+function FixedUpdate() {
+	var moveHorizontal : float = Input.GetAxis("Horizontal");
+	var moveVertical : float = Input.GetAxis("Vertical");	
+	var movement : Vector3 = new Vector3(moveHorizontal, 0.0f, moveVertical);
+	rigidbody.AddForce(movement * speed * Time.deltaTime);
+	
+}
+
+function OnTriggerEnter( other: Collider) {
+	if(other.gameObject.tag == "Pick Up") {
+		other.gameObject.SetActive(false);
+		count++;
+		SetCountText();
+	};
+
+}
+
+function SetCountText() {
+	countText.text = "Count: " + count;
+	if(count >= 7) {
+		winText.text = "You WIN!";
+	}
+}
